790177935ed0a4c37cdc4e973ef2e27eaae9e6e0
bussiere <bussiere@gmail.com>
2022-03-22 17:13:40 +0100

    wip master Updated: 2022-03-22 17:13:40

--- commit.b.sh
+++ commit.b.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 git pull origin $branch
-git add .
-current="`date +'%Y-%m-%d %H:%M:%S'`"
-msg="Updated: $current"
-git commit -m "wip $branch $msg"
-git push origin $branch
-hs=`hostname`
 echo $hs
 if [ ${hs} = 'kusanagi' ]; then
 python3 version.py
@@ -14,3 +8,8 @@ npm publish
 else
 echo "no  publishing no kusanagi"
 fi
+git add .
+current="`date +'%Y-%m-%d %H:%M:%S'`"
+msg="Updated: $current"
+git commit -m "wip $branch $msg"
+git push origin $branch
