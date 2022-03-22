dcdb42eac6b52f87a6ab7cf61ff8e2f3ede42e78
bussiere <bussiere@gmail.com>
2022-03-22 18:12:07 +0100

    wip master Updated: 2022-03-22 18:12:07

--- commit.b.sh
+++ commit.b.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 git pull origin $branch
+
 hs=`hostname`
 echo $hs
 if [ ${hs} = 'kusanagi' ]; then
 git config --global user.name "bussiere"
 git config --global user.email "bussiere@gmail.com"
-python3 version.py
-npm publish
 else
 echo "no  publishing no kusanagi"
 fi
+python3 version.py
+npm publish
 git add .
 current="`date +'%Y-%m-%d %H:%M:%S'`"
 msg="Updated: $current"
