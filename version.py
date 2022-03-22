5325b407a46c01e03cd1ef6507a3393bc4872f93
bussiere <bussiere@gmail.com>
2022-03-22 17:03:28 +0100

    wip master Updated: 2022-03-22 17:03:28

--- version.py
+++ version.py
@@ -7,7 +7,7 @@ with open('package.json') as json_data:
     version = str(d['version'].split(".")[0]+"_"+''.join(d['version'].split(".")[0:]))
     version = float(version.replace("_","."))
     print(version)
-    d['version'] = str(version+0.000001)
+    d['version'] = str(version+0.01)[0:4]
     
 
 with open('package.json', 'w') as outfile:
