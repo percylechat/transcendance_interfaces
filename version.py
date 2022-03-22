e1f4866e10b539030c48f769f7f28821bc573aa0
bussiere <bussiere@gmail.com>
2022-03-22 17:22:37 +0100

    wip master Updated: 2022-03-22 17:22:37

--- version.py
+++ version.py
@@ -4,7 +4,7 @@ import json
 with open('package.json') as json_data:
     d = json.load(json_data)
     d['version'] = str(d['version'])
-    version = str(d['version'].split(".")[0]+"_"+''.join(d['version'].split(".")[0:]))
+    version = str(d['version'].split(".")[0]+"_"+''.join(d['version'].split(".")[1:]))
     version = float(version.replace("_","."))
     print("version")
     print(version)
